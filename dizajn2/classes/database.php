8c4ede78c36e17a842a969e49c216531867c2454
mrsZemiak <64277932+mrsZemiak@users.noreply.github.com>
2020-12-10 14:48:02 +0100

    Add files via upload

---
+++ dizajn2/classes/database.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace classes;
+
+class database
+{
+    /**
+     * @var string
+     */
+    private $host = "localhost";
+
+    /**
+     * @var string
+     */
+    private $username = "root";
+
+    /**
+     * @var string
+     */
+    private $password = "";
+    /**
+     * @var string
+     */
+    private $dbName = "";
+    /**
+     * @var int
+     */
+    private $port;
+    /**
+     * @var \PDO
+     */
+    private $connection;
+
+    /**
+     * database constructor.
+     * @param $host
+     * @param $username
+     * @param $password
+     * @param $dbName
+     * @param int $port
+     */
+    public function __construct($host, $username, $password, $dbName, $port = 3306)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbName = $dbName;
+        $this->port = $port;
+
+        try {
+            $connection = new \PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName . ";port=" . $this->port, $this->username, $this->password);
+            $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->connection = $connection;
+
+        } catch (\PDOException $exception) {
+            echo "Error while database connect " . $exception->getMessage();
+        }
+    }
+
+    /**
+     * @return \PDO
+     */
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * @param $conn
+     */
+
+    public function setConnection($conn)
+    {
+        if ($conn instanceof \PDO) {
+            $this->connection = $conn;
+        }
+    }
+
+    /**
+     * @return array
+     */
+
+    public function getMenuStuffs($session)
+    {
+        if(!empty($session)){
+            $sql = "SELECT * FROM menu WHERE menu.logged_in = 1";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        }else{
+            $sql = "SELECT * FROM menu WHERE menu.logged_in = 0";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }
+
+    }
+
+
+    /**
+     * @param $firstname
+     * @param $lastname
+     * @param $email
+     * @param $password
+     */
+
+    public function register($firstname, $lastname, $email, $password)
+    {
+        $exists = false;
+        $link = mysqli_connect('localhost', 'root', '', 'todos', 3306);
+
+        $mysql_get_user = "SELECT * FROM users WHERE email='$email'";
+        mysqli_select_db($link, 'todos');
+        $retval = mysqli_query($link, $mysql_get_user);
+
+        while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
+            $exists = true;
+        }
+        if ($exists = true) {
+            $message = "Tento používateľ už existuje!";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+
+        } else {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (meno,priezvisko,email,password) VALUES ('" . $firstname . "','" . $lastname . "','" . $email . "','" . $hashed_password . "')";
+
+            $stmtinsert = $this->connection->prepare($sql);
+            $stmtinsert->execute();
+            if ($stmtinsert) {
+                echo "Succesfull saved";
+            } else {
+                "Error";
+            }
+        }
+    }
+
+    /**
+     * @param $email
+     * @param $password
+     */
+
+    public function login($email, $password)
+    {
+        $link = mysqli_connect('localhost', 'root', '', 'todos', 3306);
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if (empty($email) || empty($password)){
+            $message = "Nesprávne meno alebo heslo";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+        }else{
+            $sql = "SELECT * FROM users WHERE idusers =? || email=?;";
+            $stmt = mysqli_stmt_init($link);
+            if (!mysqli_stmt_prepare($stmt,$sql)){
+                $message = "Nesprávne meno";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+            }else{
+                mysqli_stmt_bind_param($stmt,"ss",$email,$email);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                if ($row = mysqli_fetch_assoc($result)){
+
+                    $pwdCheck = password_verify($password, $row['password']);
+                    if ($pwdCheck == false){
+
+                        $message = "Nesprávne heslo";
+                        echo "<script type='text/javascript'>alert('$message');</script>";
+                    }
+                    elseif ($pwdCheck == true){
+                       // session_start();
+                        $_SESSION['userId'] = $row['idusers'];
+                        $_SESSION['userUID'] = $row['meno'];
+//                        $message = "System in";
+//                        echo "<script type='text/javascript'>alert('$message');</script>";
+                            header("location: todo.php");
+                            exit();
+                    }else{
+                        $message = "Nesprávne heslo";
+                        echo "<script type='text/javascript'>alert('$message');</script>";
+                    }
+
+                }else{
+                    $message = "Používateľ neexistuje";
+                    echo "<script type='text/javascript'>alert('$message');</script>";
+                }
+
+            }
+        }
+    }
+
+    public function addTask($title)
+    {
+        $link = mysqli_connect('localhost', 'root', '', 'todos', 3306);
+        $title = $title;
+
+
+        $stmt = $link->prepare("INSERT INTO todolists (title,idusers) VALUES('" . $title . "','" . $_SESSION['userId'] . "')");
+        $stmt->execute();
+    }
+
+                public function addTaskForList($task,$id_list)
+    {
+        $link = mysqli_connect('localhost', 'root', '', 'todos', 3306);
+
+
+                $stmt = $link->prepare("INSERT INTO tasks (task, id_todo) VALUES('".$task."','".$id_list."')");
+                $stmt->execute();
+       }
+
+    public function displayToDo($id){
+
+       $sql = "SELECT * FROM todolists WHERE idusers =".$id;
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+}
+    public function displayTasks($id){
+
+        $sql = "SELECT * FROM tasks WHERE id_todo =".$id." ORDER BY created DESC";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function deleteTask ($id){
+        $sql = "DELETE FROM tasks WHERE id_task = ".$id;
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute();
+    }
+    public function deleteTodo ($id){
+        $sql = "DELETE FROM todolists WHERE idtodo = ".$id;
+        $sql2 = "DELETE FROM tasks WHERE id_todo = ".$id;
+
+        $stmt = $this->connection->prepare($sql2);
+        $stmt2 = $this->connection->prepare($sql);
+
+
+        $stmt->execute();
+        $stmt2->execute();
+       return $stmt2;
+
+    }
+    public function completeTask ($id){
+        $sql = "UPDATE tasks SET checked = 1 WHERE id_task = ".$id;
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute();
+    }
+
+}
+?>
---
+++ dizajn2/header.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+?>
+<title>PotaToDo</title>
+<link rel="shortcut icon" href="images/potato.ico.ico" />
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+<link rel="stylesheet" href="assets/css/main.css" />
+<noscript><link rel="stylesheet" href="assets/css/noscript.css"/></noscript>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
